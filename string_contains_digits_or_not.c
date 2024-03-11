dbe306c9f5252edf274193632c4637e6496be34c
divya1829 <147473180+divya1829@users.noreply.github.com>
2024-03-11 19:40:31 +0530

    #include<stdio.h> #include<string.h> int main(){     int t;     scanf("%d",&t);     while(t--){         char str[1000];         int flag = 0;         scanf("%s",str);         int x=strlen(str);         for(int i=0;i<x;i++) {             if(str[i]>='0'&& str[i]<='9' ){                 flag = 1;                 break;             }         }         if(flag == 1)         printf("Yes ");         else         printf("No ");     } }

---
+++ string_contains_digits_or_not.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    int t;
+    scanf("%d",&t);
+    while(t--){
+        char str[1000];
+        int flag = 0;
+        scanf("%s",str);
+        int x=strlen(str);
+        for(int i=0;i<x;i++) {
+            if(str[i]>='0'&& str[i]<='9' ){
+                flag = 1;
+                break;
+            }
+        }
+        if(flag == 1) 
+        printf("Yes
+");
+        else
+        printf("No
+");
+    }
+}
